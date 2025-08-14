c58a8651f429491504a25ff2e9f1bc455a22e438
Jose Emilio <silvagoisemilio@gmail.com>
2025-08-14 12:39:42 -0300

    [feat] Logo in App.js

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import logo from './assets/pngegg.png';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <image source={logo} style={styles.logo}/>
+      </View>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo:{
+    heigth:128,
+    width:128,
+  }
+});
